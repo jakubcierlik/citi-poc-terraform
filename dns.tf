56136f465f36423108d328ae0f93e1bb843addfe
Jakub Cierlik <jakub.cierlik@amartus.com>
2021-04-12 15:18:31 +0200

    fix

--- dns.tf
+++ dns.tf
@@ -7,7 +7,7 @@ resource "aws_route53_zone" "main_zone" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.main_zone.zone_id
   name    = var.domain_name
   type    = "A"
   ttl     = "300"
