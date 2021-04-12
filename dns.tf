dc3221fee16aa47d31a2204a8c56f28ada00e7f9
Jakub Cierlik <jakub.cierlik@amartus.com>
2021-04-12 14:57:00 +0200

    fixes

--- dns.tf
+++ dns.tf
@@ -6,11 +6,11 @@ resource "aws_route53_zone" "main_zone" {
   }
 }
 
-resource "aws_route53_record" "main_zone_record" {
-  zone_id = aws_route53_zone.main_zone.zone_id
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
   name    = var.domain_name
-  type    = "NS"
+  type    = "A"
   ttl     = "300"
-  records = [ var.domain_name ]
-  # records = aws_route53_zone.dev.name_servers
+  records = [var.domain_name]
 }
--- vpn.tf
+++ vpn.tf
@@ -1,28 +1,24 @@
-# # TODO: sort out networking - vpc and subnet with DB VM and openshift clusters
-# # resource "aws_vpc" "vpc" {
-# #   cidr_block = "10.0.0.0/16"
-# # }
+# TODO: sort out networking - vpc and subnet with DB VM and openshift clusters
+resource "aws_vpn_gateway" "vpn_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+}
 
-# resource "aws_vpn_gateway" "vpn_gateway" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_customer_gateway" "customer_gateway" {
+  bgp_asn    = 65000
+  ip_address = "160.0.0.1"
+  type       = "ipsec.1"
+}
+
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id      = aws_vpn_gateway.vpn_gateway.id
+  customer_gateway_id = aws_customer_gateway.customer_gateway.id
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
+
+# examplary specific route if needed
+# resource "aws_vpn_connection_route" "office" {
+#   destination_cidr_block = "192.168.10.0/24"
+#   vpn_connection_id      = aws_vpn_connection.main.id
 # }
-
-# resource "aws_customer_gateway" "customer_gateway" {
-#   bgp_asn    = 65000
-#   ip_address = "160.0.0.1"
-#   type       = "ipsec.1"
-# }
-
-# resource "aws_vpn_connection" "main" {
-#   vpn_gateway_id      = aws_vpn_gateway.vpn_gateway.id
-#   customer_gateway_id = aws_customer_gateway.customer_gateway.id
-#   type                = "ipsec.1"
-#   static_routes_only  = true
-# }
-
-# # examplary specific route if needed
-# # resource "aws_vpn_connection_route" "office" {
-# #   destination_cidr_block = "192.168.10.0/24"
-# #   vpn_connection_id      = aws_vpn_connection.main.id
-# # }
 
